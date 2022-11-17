1ca3f0b1ab9f37acdd46454d272bbfeb8994ffa2
AminRicham <110353705+AminRicham@users.noreply.github.com>
2022-11-17 10:18:53 -0300

    Update Main.java

--- src/sistemaBanco_main/Main.java
+++ src/sistemaBanco_main/Main.java
@@ -1,24 +1,68 @@
 package sistemaBanco_main;
-
+import java.util.Scanner;
 import sistemaBanco.Conta;
 
 public class Main {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
 		Conta c1, c2;
 		c1 = new Conta(1, "Vitor", 2, 150, 4);
 		c2 = new Conta(2, "Lucas", 2, 100, 3);
 		
-		//TESTE DE IMPRESSAO
+		/*
+		//TESTE DE IMPRESSAO e FUNCOES
 		System.out.println("conta1:\n"+c1);
 		System.out.println("\nConta2:\n"+c2); 
 		
 		//Depositar(c1, 50);
 		//Sacar(c2,-4);
-		Transferir(c1,c2, 32);
+		//Transferir(c1,c2, 32);
 		
 		System.out.println("\nNova conta N1:\n"+c1);
 		System.out.println("\nnova conta:\n"+c2);
+		*/
+		
+		float conta;
+		System.out.println("Qual o numero de sua conta?")
+		conta = sc.extInt();
+		if (conta != c1.getNumeroConta() || conta.getNumeroConta() != c2)
+			System.out.println("Conta invalida");
+		else{
+		
+		int escolha;
+		System.out.println("Boa tarde, insira a operacao desejada, sendo elas
+				   1-Sacar, 2-Depositar, 3-Transferir valor, 4-Consultar saldo");
+		float valor;	   
+		switch(escolha){
+			case 1:
+			System.out.println("Insira o valor a ser sacado de sua conta:")
+			valor = sc.nextFloat();
+			Sacar(conta,valor);
+			sc.nextFloat:
+			
+			case 2:
+			System.out.println("Insira o valor a ser depositado em sua conta:");
+			valor = sc.nextFloat();
+			Depositar(conta,valor);
+			sc.nextFloat();
+			
+			case 3:
+			System.out.println("Insira a conta a ser depositada");
+			conta2 = sc.nextInt();
+			System.out.println("Insira o valor a ser depositado:");
+			valor = sc.nextFloat();
+			//FAZER CHECAGEM DE CONTA2
+			Transferir(conta1,conta2, valor);
+			
+			case 4:
+			System.out.println("O saldo da conta "+conta1+" é:"+ conta1.getSaldo());
+			
+		}//else		
+		
+		
+		
 	}
 	
 	//DEPOSITO
