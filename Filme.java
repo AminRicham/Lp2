712f6df69bbb944074ebffb32b23bc524a12e4b5
AminRicham <110353705+AminRicham@users.noreply.github.com>
2022-12-16 11:43:55 -0300

    Create Filme.java

---
+++ Filme.java
@@ -0,0 +1,18 @@
+
+public class Filme(){
+  private String tit;
+  private String desc;
+  private Diretor dir;
+  private float minutos;
+  
+  public void exibirDuracaoEmHoras(){
+    //float horas;
+    //horas = time/60;
+    System.out.println(“O filme "+tit+" possui"+time/60" horas e "+time%60+" minutos de duração”);
+  }
+  //Construtores
+  //Get e set
+  //toString
+  
+  
+}
